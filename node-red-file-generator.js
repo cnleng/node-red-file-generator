4f6301e5f0ce115b2d9e55af25696bd6e22df85f
labadmin <labadmin@scva157.lss.emc.com>
2018-09-11 12:43:21 -0400

    Fix Bugs & Errors

--- node-red-file-generator.js
+++ node-red-file-generator.js
@@ -1,44 +1,31 @@
 module.exports = function(RED) {
     var rfs = require('rotating-file-stream');
-    var stream = rfs('file.log', {
-        path: '/opt/node-red/ksi'
-        size:     '10M', // rotate every 10 MegaBytes written
-        interval: '15m',  // rotate daily
+    var stream = rfs('sensor-data.log', {
+        path: '/opt/node-red/ksi',
+        size:     '1M', // rotate every 10 MegaBytes written
+        interval: '2s'  // rotate daily
+        //rotationTime: 'true'
     });
     function NodeRedFileGenerator(config) {
         RED.nodes.createNode(this,config);
         var node = this;
         node.on('input', function(msg) {
             //msg.payload = msg.payload.toLowerCase();
-            stream.on('error', function(err) {
-    // here are reported blocking errors
-    // once this event is emitted, the stream will be closed as well
-});
- 
-stream.on('open', function(filename) {
-    // no rotated file is open (emitted after each rotation as well)
-    // filename: useful if immutable option is true
-});
- 
-stream.on('removed', function(filename, number) {
-    // rotation job removed the specified old rotated file
-    // number == true, the file was removed to not exceed maxFiles
-    // number == false, the file was removed to not exceed maxSize
-});
- 
-stream.on('rotation', function() {
-    // rotation job started
-});
- 
-stream.on('rotated', function(filename) {
-    // rotation job completed with success producing given filename
-});
- 
-stream.on('warning', function(err) {
-    // here are reported non blocking errors
-});
+            var stream = rfs('sensor-data.log', {
+                path: '/opt/node-red/ksi',
+                size:     '1M', // rotate every 10 MegaBytes written
+                interval: '1m',  // rotate daily         
+                rotationTime: 'true'     });
+            stream.write(msg.payload);
+            stream.end();
+
+	    stream.on('error', function(err) {
+            // here are reported blocking errors
+            // once this event is emitted, the stream will be closed as well
+            node.log(err);
+            });
             node.send(msg);
-        });
+       });
     }
     RED.nodes.registerType("node-red-file-generator",NodeRedFileGenerator);
 }
